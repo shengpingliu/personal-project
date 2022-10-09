bdd1d0f23d9800c45a45cde2a693864828d53f86
shengpingliu <shengping1103@gmail.com>
2022-10-09 17:16:49 -0500

    Add click action to flip card

--- amz-rating/src/components/Star.js
+++ amz-rating/src/components/Star.js
@@ -1,28 +1,19 @@
 import "./Star.css";
 
-const Star = (props) => {
+const Star = ({ rating, starId, onMouseEnter, onMouseLeave, onClick }) => {
   let styleClass = "star-rating-blank";
-  if (props.rating >= props.starId) {
+  if (rating >= starId) {
     styleClass = "star-rating-fill";
   }
   return (
     <div
-      onMouseEnter={props.onMouseEnter}
-      onMouseLeave={props.onMouseLeave}
-      onClick={props.onClick}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onClick={onClick}
     >
-      <svg
-        height="55px"
-        width="53px"
-        className={styleClass}
-        viewBox="0 0 25 23"
-        data-rating="1"
-      >
-        <polygon
-          strokeWidth="0"
-          points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78"
-        />
-      </svg>
+      <div className={styleClass}>
+        <i className="star-icon"></i>
+      </div>
     </div>
   );
 };
